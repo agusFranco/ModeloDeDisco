86221352f82ce9905f7a73c3bd596321318ffbd1
Agustin Franco <francoagustinfacundo@gmail.com>
2019-04-28 18:57:10 -0300

    Manejo de ArgumentNullException - UnitTest - Redondeo

--- src/disco/Disco.java
+++ src/disco/Disco.java
@@ -1,101 +1,73 @@
 package disco;
 
 public class Disco {
-	
-	private float radioInt;
-	private float radioExt;
-	public double perimetroExt = 0;
-	public double perimetroInt = 0;
-	public double superficie = 0;
 
+	private double radioInt;
+	private double radioExt;
+	private double perimetroExt;
+	private double perimetroInt;
+	private double superficie;
 
+	public Disco(double radioInterior, double radioExterior) {
+		if(radioInterior == 0 || radioExterior == 0) {
+			throw new IllegalArgumentException("Los radios deben ser mayores a 0.");
+		}
+		
+		if(radioInterior > radioExterior) {
+			throw new IllegalArgumentException("El radio interior debe ser menor al radio exterior.");
+		}
+		
+		this.setRadioExt(radioExterior);
+		this.setRadioInt(radioInterior);
+	}
 
-public void Disco(float radioInterior,float radioExterior) {
-	
-	this.setRadioInt(radioInterior);
-	this.setRadioExt(radioExterior);
-	this.setRadioExt(radioExterior);
-	this.setRadioInt(radioInterior);
-	this.setPerimetroExt(this.calculoDelPerimetroExterior(radioExterior));
-	this.setPerimetroInt(this.calculoDelPerimetroInterior(radioInterior));
-	this.setSuperficie(this.calculoDeLaSuperficie(radioExterior));	
-}
+	public double getRadioInt() {
+		return radioInt;
+	}
 
+	public void setRadioInt(double radioInt) {
+		this.radioInt = radioInt;
+		this.calculoDelPerimetroInterior();
+		this.calculoDeLaSuperficie();
+	}
 
-// Calculo de Perimetros y Superficie
+	public double getRadioExt() {
+		return radioExt;
+	}
 
+	public void setRadioExt(double radioExt) {
+		this.radioExt = radioExt;
+		this.calculoDelPerimetroExterior();
+		this.calculoDeLaSuperficie();
+	}
 
+	public double getPerimetroExt() {
+		return perimetroExt;
+	}
 
+	public double getPerimetroInt() {
+		return perimetroInt;
+	}
 
-public double calculoDelPerimetroExterior(double radioParaPerimetroExterior) {
-	
-	double perimetroExterior = radioParaPerimetroExterior * 2 * Math.PI;
-	return perimetroExterior;
-	
-}
+	public double getSuperficie() {
+		return superficie;
+	}
 
-public double calculoDelPerimetroInterior(double radioParaPerimetroInterior) {
-	
-	double perimetroInterior = radioParaPerimetroInterior * 2 * Math.PI;
-	return perimetroInterior;
-}
+	private void calculoDelPerimetroExterior() {
+		// Redondeo a 2 decimales.
+		this.perimetroExt = Math.round(this.radioExt * 2 * Math.PI * 100d) / 100d;
+	}
 
-public double calculoDeLaSuperficie(double radioExteriorParaLaSuperficie) {
-	
-	double Superficie = Math.PI * radioExteriorParaLaSuperficie * radioExteriorParaLaSuperficie;
-	return Superficie;
+	private void calculoDelPerimetroInterior() {
+		// Redondeo a 2 decimales.
+		this.perimetroInt = Math.round(this.radioInt * 2 * Math.PI * 100d) / 100d;
+	}
 
+	private void calculoDeLaSuperficie() {
+		// Redondeo a 2 decimales.
+		double superficieExt = Math.round(Math.PI * this.radioExt * this.radioExt * 100d) / 100d;
+		double superficieInt = Math.round(Math.PI * this.radioInt * this.radioInt * 100d) / 100d;
 
-}	
-
-
-//Getters and Setters
-
-public void setSuperficie(double superficie) {
-	this.superficie = superficie;
-}
-
-public void setPerimetroExt(double perimetroExt) {
-	this.perimetroExt = perimetroExt;
-}
-
-public void setPerimetroInt(double perimetroInt) {
-	this.perimetroInt = perimetroInt;
-}
-
-public void setRadioInt(float radioInt) {
-	this.radioInt = radioInt;
-}
-
-public float getRadioInt() {
-	return radioInt;
-}
-
-
-public float getRadioExt() {
-	return radioExt;
-}
-
-
-public double getPerimetroExt() {
-	return perimetroExt;
-}
-
-
-public double getPerimetroInt() {
-	return perimetroInt;
-}
-
-
-public double getSuperficie() {
-	return superficie;
-}
-
-
-public void setRadioExt(float radioExt) {
-	this.radioExt = radioExt;
-}
-
-
-
+		this.superficie = Math.round((superficieExt - superficieInt) * 100d) / 100d;
+	}
 }
--- test/discoTest/DiscoTests.java
+++ test/discoTest/DiscoTests.java
@@ -1,5 +1,191 @@
 package discoTest;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import disco.Disco;
+
 public class DiscoTests {
 
+	@Test(expected = IllegalArgumentException.class)
+	public void noSeCreaUnDiscoPorRadiosCero() {
+		double radioInt = 0;
+		double radioExt = 0;
+
+		Disco disco = new Disco(radioInt, radioExt);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void noSeCreaUnDiscoPorRadiosIntMayorARadioExt() {
+		double radioInt = 2;
+		double radioExt = 1;
+
+		Disco disco = new Disco(radioInt, radioExt);
+	}
+
+	@Test
+	public void seCreaUnDisco() {
+		double radioInt = 2;
+		double radioExt = 3;
+
+		Disco disco = new Disco(radioInt, radioExt);
+		Assert.assertTrue(disco != null);
+	}
+
+	@Test
+	public void seCreaUnDiscoConRadioIntDosYRadioExtTres() {
+		double radioInt = 2;
+		double radioExt = 3;
+
+		Disco disco = new Disco(radioInt, radioExt);
+		Assert.assertTrue(disco.getRadioInt() == radioInt);
+		Assert.assertTrue(disco.getRadioExt() == radioExt);
+	}
+
+	@Test
+	public void seActualizaElRadioInterior() {
+		double radioInt = 2;
+		double radioExt = 3;
+		double radioIntEsperado = 4;
+
+		Disco disco = new Disco(radioInt, radioExt);
+		disco.setRadioInt(radioIntEsperado);
+
+		Assert.assertTrue(disco.getRadioInt() == radioIntEsperado);
+	}
+
+	@Test
+	public void seActualizaElRadioExterior() {
+		double radioInt = 2;
+		double radioExt = 3;
+		double radioExtEsperado = 4;
+
+		Disco disco = new Disco(radioInt, radioExt);
+		disco.setRadioExt(radioExtEsperado);
+
+		Assert.assertTrue(disco.getRadioExt() == radioExtEsperado);
+	}
+
+	@Test
+	public void seObtieneElPerimetroInterior() {
+		double radioInt = 2;
+		double radioExt = 3;
+		double perimetroEsperado = 12.57;
+
+		Disco disco = new Disco(radioInt, radioExt);
+
+		double perimetroObtenido = disco.getPerimetroInt();
+
+		Assert.assertTrue(perimetroObtenido == perimetroEsperado);
+	}
+
+	@Test
+	public void seObtieneElPerimetroExterior() {
+		double radioInt = 2;
+		double radioExt = 3;
+		double perimetroEsperado = 18.85;
+
+		Disco disco = new Disco(radioInt, radioExt);
+
+		double perimetroObtenido = disco.getPerimetroExt();
+
+		Assert.assertTrue(perimetroObtenido == perimetroEsperado);
+	}
+
+	@Test
+	public void seObtieneLaSuperficie() {
+		double radioInt = 2;
+		double radioExt = 3;
+		double superficieEsperada = 15.7;
+
+		Disco disco = new Disco(radioInt, radioExt);
+
+		double superficieObtenida = disco.getSuperficie();
+
+		Assert.assertTrue(superficieObtenida == superficieEsperada);
+	}
+
+	@Test
+	public void alAcualizarRadioIntCambiaElPerimetroInt() {
+		double radioInt = 2d;
+		double radioExt = 3d;
+		double radioIntNuevo = 4d;
+		double perimetroInicialEsperado = 12.57d;
+		double perimetroFinalEsperado = 25.13d;
+
+		Disco disco = new Disco(radioInt, radioExt);
+
+		double perimetroInicialObtenido = disco.getPerimetroInt();
+
+		Assert.assertTrue(perimetroInicialObtenido == perimetroInicialEsperado);
+
+		disco.setRadioInt(radioIntNuevo);
+
+		double perimetroFinalObtenido = disco.getPerimetroInt();
+
+		Assert.assertTrue(perimetroFinalObtenido == perimetroFinalEsperado);
+	}
+
+	@Test
+	public void alAcualizarRadioExtCambiaElPerimetroExt() {
+		double radioInt = 2d;
+		double radioExt = 3d;
+		double radioExtNuevo = 5d;
+		double perimetroInicialEsperado = 18.85d;
+		double perimetroFinalEsperado = 31.42d;
+
+		Disco disco = new Disco(radioInt, radioExt);
+
+		double perimetroInicialObtenido = disco.getPerimetroExt();
+
+		Assert.assertTrue(perimetroInicialObtenido == perimetroInicialEsperado);
+
+		disco.setRadioExt(radioExtNuevo);
+
+		double perimetroFinalObtenido = disco.getPerimetroExt();
+
+		Assert.assertTrue(perimetroFinalObtenido == perimetroFinalEsperado);
+	}
+
+	@Test
+	public void alActualizarRadioIntCambiaLaSuperficie() {
+		double radioInt = 2d;
+		double radioExt = 6d;
+		double radioIntNuevo = 4d;
+		double superficieInicialEsperada = 100.53d;
+		double superficieFinalEsperada = 62.83d;
+
+		Disco disco = new Disco(radioInt, radioExt);
+
+		double superficieInicialObtenida = disco.getSuperficie();
+
+		Assert.assertTrue(superficieInicialObtenida == superficieInicialEsperada);
+
+		disco.setRadioInt(radioIntNuevo);
+
+		double superficieFinalObtenida = disco.getSuperficie();
+
+		Assert.assertTrue(superficieFinalObtenida == superficieFinalEsperada);
+	}
+
+	@Test
+	public void alActualizarRadioExtCambiaLaSuperficie() {
+		double radioInt = 2d;
+		double radioExt = 6d;
+		double radioExtNuevo = 8d;
+		double superficieInicialEsperada = 100.53d;
+		double superficieFinalEsperada = 188.49d;
+
+		Disco disco = new Disco(radioInt, radioExt);
+
+		double superficieInicialObtenida = disco.getSuperficie();
+
+		Assert.assertTrue(superficieInicialObtenida == superficieInicialEsperada);
+
+		disco.setRadioExt(radioExtNuevo);
+
+		double superficieFinalObtenida = disco.getSuperficie();
+
+		Assert.assertTrue(superficieFinalObtenida == superficieFinalEsperada);
+	}
 }
